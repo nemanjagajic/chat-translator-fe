d8a3ff07167d288176410e37ecd6c6728d1070d4
Nemanja Gajic <nemanja@MacBook-Pro-2.local>
2022-03-25 19:43:55 +0100

    added scroll-y for chats list

--- components/chats/ChatsList.tsx
+++ components/chats/ChatsList.tsx
@@ -8,9 +8,9 @@ type ChatsListProps = {
 
 const ChatsList: FC<ChatsListProps> = ({ chats = [] }) => {
   return (
-    <>
+    <div className='flex flex-col h-full w-full overflow-y-scroll'>
       {chats.map(chat => <ChatItem key={chat._id} chat={chat} />)}
-    </>
+    </div>
   )
 }
 
