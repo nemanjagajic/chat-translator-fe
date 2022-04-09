dfd044655ad52817e6d38bb1bf8eef430d94eb4c
Nemanja Gajic <nemanja@MacBook-Pro-2.local>
2022-04-09 18:42:03 +0200

    added test for auth hook

---
+++ __tests__/hooks/auth.test.ts
@@ -0,0 +1,34 @@
+import { useAuthRedirection } from '../../hooks/auth'
+import { renderHook } from '@testing-library/react-hooks'
+import { useRouter } from 'next/router'
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn()
+}))
+const useRouterMock = useRouter as jest.Mock
+
+describe('Auth hooks', () => {
+  it('verifies useAuthRedirection redirects to auth when no user is logged in', () => {
+    let currentPath = ''
+    jest.spyOn(window.localStorage.__proto__, 'getItem').mockImplementation(() => null)
+    useRouterMock.mockImplementation(() => ({
+      replace: (path: string) => { currentPath = path },
+      pathname: currentPath
+    }))
+    renderHook(useAuthRedirection)
+    expect(currentPath).toBe('/auth')
+  })
+
+  it('verifies useAuthRedirection redirects to home when user is logged in', () => {
+    let currentPath = '/auth'
+    jest.spyOn(window.localStorage.__proto__, 'getItem').mockImplementation(() => (
+      '{ email: \'test@gmail.com\', firstName: \'test\', lastName: \'test\' }'
+    ))
+    useRouterMock.mockImplementation(() => ({
+      replace: (path: string) => { currentPath = path },
+      pathname: currentPath
+    }))
+    renderHook(useAuthRedirection)
+    expect(currentPath).toBe('/')
+  })
+})
