cc250f7d3d8a1f8b1184bcb933541a35dfe0fb13
Nemanja Gajic <nemanja@MacBook-Pro-2.local>
2022-04-09 16:20:36 +0200

    auth page test improved

--- __tests__/pages/auth.test.tsx
+++ __tests__/pages/auth.test.tsx
@@ -8,10 +8,17 @@ jest.mock('../../hooks/i18n', () => ({
 }))
 
 describe('Auth page', () => {
-  it('renders auth form', () => {
+  it('renders auth form with reactive input fields', () => {
+    const email = 'test@email.com'
+    const password = 'testpass'
+
     render(<Auth />)
-    screen.getByPlaceholderText(en.auth.placeholders.email)
-    screen.getByPlaceholderText(en.auth.placeholders.password)
+    const inputEmail = screen.getByPlaceholderText(en.auth.placeholders.email) as HTMLInputElement
+    const inputPassword = screen.getByPlaceholderText(en.auth.placeholders.password) as HTMLInputElement
+    fireEvent.change(inputEmail, { target: { value: email } })
+    fireEvent.change(inputPassword, { target: { value: password } })
+    expect(inputEmail.value).toBe(email)
+    expect(inputPassword.value).toBe(password)
     screen.getByText(en.auth.buttons.logIn)
     expect(screen.getByTestId('error-message').textContent).toBe('')
   })
@@ -19,7 +26,7 @@ describe('Auth page', () => {
   it('displays error message on bad request', async () => {
     render(<Auth />)
     const message = 'There has been some issues in login request'
-    const failingLoginRequest = () => Promise.reject({ response: { status: 200, data: { message } } })
+    const failingLoginRequest = () => Promise.reject({ response: { status: 401, data: { message } } })
     const logIn = jest.spyOn(authService, 'logIn').mockImplementationOnce(failingLoginRequest)
 
     fireEvent.click(screen.getByText(en.auth.buttons.logIn))
