f95cffdd9db5a8ffffe18fa0bddbcae60a911310
Nemanja Gajic <nemanja@MacBook-Pro-2.local>
2022-04-09 18:56:15 +0200

    minor refactor

--- __tests__/hooks/auth.test.ts
+++ __tests__/hooks/auth.test.ts
@@ -6,27 +6,29 @@ jest.mock('next/router')
 const useRouterMock = useRouter as jest.Mock
 
 describe('Auth hooks', () => {
-  it('verifies useAuthRedirection redirects to auth when no user is logged in', () => {
-    let currentPath = ''
-    jest.spyOn(window.localStorage.__proto__, 'getItem').mockImplementation(() => null)
-    useRouterMock.mockImplementation(() => ({
-      replace: (path: string) => { currentPath = path },
-      pathname: currentPath
-    }))
-    renderHook(useAuthRedirection)
-    expect(currentPath).toBe('/auth')
-  })
+  describe('useAuthRedirection', () => {
+    it('redirects to auth when no user is logged in', () => {
+      let currentPath = ''
+      jest.spyOn(window.localStorage.__proto__, 'getItem').mockImplementation(() => null)
+      useRouterMock.mockImplementation(() => ({
+        replace: (path: string) => { currentPath = path },
+        pathname: currentPath
+      }))
+      renderHook(useAuthRedirection)
+      expect(currentPath).toBe('/auth')
+    })
 
-  it('verifies useAuthRedirection redirects to home when user is logged in', () => {
-    let currentPath = '/auth'
-    jest.spyOn(window.localStorage.__proto__, 'getItem').mockImplementation(() => (
-      '{ email: \'test@gmail.com\', firstName: \'test\', lastName: \'test\' }'
-    ))
-    useRouterMock.mockImplementation(() => ({
-      replace: (path: string) => { currentPath = path },
-      pathname: currentPath
-    }))
-    renderHook(useAuthRedirection)
-    expect(currentPath).toBe('/')
+    it('redirects to home when user is logged in', () => {
+      let currentPath = '/auth'
+      jest.spyOn(window.localStorage.__proto__, 'getItem').mockImplementation(() => (
+        '{ email: \'test@gmail.com\', firstName: \'test\', lastName: \'test\' }'
+      ))
+      useRouterMock.mockImplementation(() => ({
+        replace: (path: string) => { currentPath = path },
+        pathname: currentPath
+      }))
+      renderHook(useAuthRedirection)
+      expect(currentPath).toBe('/')
+    })
   })
 })
