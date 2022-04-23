95a90dd0c063671e1c09af1612f44efedbe102b5
Nemanja Gajic <nemanja@MacBook-Pro-2.local>
2022-04-23 15:53:04 +0200

    updated tests

--- __tests__/components/chats/ChatItem.test.tsx
+++ __tests__/components/chats/ChatItem.test.tsx
@@ -1,26 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import { chats } from '../../__mocks__/chats'
 import ChatItem from '../../../components/chats/ChatItem'
-import React, { useState } from 'react'
-import { Chat } from '../../../ts/chats'
-import { useChatsContext } from '../../../providers/ChatsProvider'
-
-jest.mock('../../../providers/ChatsProvider')
-const useChatsContextMock = useChatsContext as jest.Mock
+import React from 'react'
 
 describe('ChatItem component', () => {
-  it ('toggles active class on chat item selection', () => {
-    const chat = chats[0]
+  it ('renders active chat when chat is selected', () => {
     const activeChatClass = 'bg-gray-300'
-    useChatsContextMock.mockImplementation(() => {
-      const [activeChat, setActiveChat] = useState<Chat | null>(null)
-      return { selectedChat: activeChat, setSelectedChat: (chat: Chat) => setActiveChat(chat) }
-    })
 
-    render(<ChatItem chat={chat} />)
+    render(<ChatItem chat={chats[0]} selectedChat={chats[0]} setSelectedChat={() => {}} />)
+    const chatItem = screen.getByTestId('chat-item')
+    expect(chatItem).toHaveClass(activeChatClass)
+  })
+
+  it ('renders inactive chat when chat not selected', () => {
+    const activeChatClass = 'bg-gray-300'
+
+    render(<ChatItem chat={chats[0]} selectedChat={chats[1]} setSelectedChat={() => {}} />)
     const chatItem = screen.getByTestId('chat-item')
     expect(chatItem).not.toHaveClass(activeChatClass)
-    chatItem.click()
-    expect(chatItem).toHaveClass(activeChatClass)
   })
 })
