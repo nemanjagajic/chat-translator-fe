c1f3b2c7d0325df9a185982cc4a6087e63ce0d8e
Nemanja Gajic <nemanja@MacBook-Pro-2.local>
2022-04-23 13:48:51 +0200

    removed unused imports

--- components/messages/MessagesList.tsx
+++ components/messages/MessagesList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC } from 'react'
 import { Message as IMessage } from '../../ts/messages'
 import Message from './Message'
 import { useLocale } from '../../hooks/i18n'
