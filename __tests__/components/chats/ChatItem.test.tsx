88402721fac39f3457182bb9c86b356ca15fb6a7
Nemanja Gajic <nemanja@MacBook-Pro-2.local>
2022-04-23 15:54:58 +0200

    minor refactor

--- __tests__/components/chats/ChatItem.test.tsx
+++ __tests__/components/chats/ChatItem.test.tsx
@@ -6,7 +6,6 @@ import React from 'react'
 describe('ChatItem component', () => {
   it ('renders active chat when chat is selected', () => {
     const activeChatClass = 'bg-gray-300'
-
     render(<ChatItem chat={chats[0]} selectedChat={chats[0]} setSelectedChat={() => {}} />)
     const chatItem = screen.getByTestId('chat-item')
     expect(chatItem).toHaveClass(activeChatClass)
@@ -14,7 +13,6 @@ describe('ChatItem component', () => {
 
   it ('renders inactive chat when chat not selected', () => {
     const activeChatClass = 'bg-gray-300'
-
     render(<ChatItem chat={chats[0]} selectedChat={chats[1]} setSelectedChat={() => {}} />)
     const chatItem = screen.getByTestId('chat-item')
     expect(chatItem).not.toHaveClass(activeChatClass)
